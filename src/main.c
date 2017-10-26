35adfffb403de18ec3e40f88559f72311fe46dc1
h0g <rodrigue.geis@icloud.com>
2017-10-26 17:50:50 +0200

    little refactoring

--- src/main.c
+++ src/main.c
@@ -24,22 +24,22 @@ int main(int argc, char * argv[])
     // --help
     // --lang=en
     // --no-easter
-    	///variables
-	SDL_Renderer * renderer = NULL; 
+    	///variable
+	SDL_Renderer * renderer = NULL;
 	int State = 0;
 	SDL_Surface * ecran = NULL;
 	SDL_Window * fenetre = NULL;
-	///fin variables
-	
+	///fin variable
+
 	if(SDL_Init(SDL_INIT_VIDEO) != 0){
 	//on lance la sdl
 		fprintf(stdout, "echec de l'init sdl (%s)\n",SDL_GetError());
 		return EXIT_FAILURE;
 	}
-	
+
 	///init fenetre
 		// on met en place la fenetre grace a SDL_Window*
-		// puis on cree la fenetre de la maniere standard 
+		// puis on cree la fenetre de la maniere standard
 		// suivante : 'fenetre = SDL_CreateWindow();'
 	fenetre = SDL_CreateWindow(
 		"Projet LTF : nothing quelquechose",
@@ -50,20 +50,20 @@ int main(int argc, char * argv[])
 		0
 	);
 	///fin init fenetre
-	
+
 	///init ecran surface
 	ecran = SDL_GetWindowSurface(fenetre);
 	// je met les caractéristique de la fenetre dans une surface
-	// car on doit se servir de surface pour ecrire du texte dans 
-	// notre fenetre 
+	// car on doit se servir de surface pour ecrire du texte dans
+	// notre fenetre
 	///fin init ecran surface
-	
+
 	///init renderer
-	renderer = SDL_CreateRenderer(fenetre, -1, 
+	renderer = SDL_CreateRenderer(fenetre, -1,
 			SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
 				     );
 	///fin init renderer
-		
+
 	// on lance notre menu le joueur va donc faire un choix dans ce menu
 	// il sera diriger soit dans le jeu, soit dans un tuto, option, ..etc
 	// ou bien quitter directement
@@ -78,17 +78,17 @@ int main(int argc, char * argv[])
 			break;
 		default :
 			// on quitte le programme egalement
-			printf("erreur de direction apres le menu\n");	
+			printf("erreur de direction apres le menu\n");
 			break;
-	} 	
-	
+	}
+
 	///libération mémoire
 	//SDL_FreeSurface(ecran); // erreur de segmentation lors de l'utilisation de ce
 				  // sous programme de libération de surface
 	SDL_DestroyWindow(fenetre);
 	SDL_DestroyRenderer(renderer);
 	///fin libération mémoire
-	
+
 	SDL_Quit();
 	return EXIT_SUCCESS;
 }
