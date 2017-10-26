56cb1b5b4c7560b6a49ed60d12b656924e875889
F3YoD <y.djama@hotmail.fr>
2017-10-26 16:55:35 +0200

    Updating compatibility sdl

--- src/ships.c
+++ src/ships.c
@@ -1,33 +1,31 @@
-#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <math.h>
+#include "image.h"
+#include "ship.h"
 
-#include "ships.h"
 
-void load_ships()
-{
-    char shipfiles_path[] = "../path/to/files";
-    // TODO define the need for a "master" file that redirects to other files
+
+void load_ship(ship_t * pS, char * pDest, SDL_Renderer * pR){
+//
+	printf("chargement du vaisseau\n");
+	pS->img.img = IMG_Load(pDest);
+	pS->img.tex = SDL_CreateTextureFromSurface(pR, pS->img.img);
+	pS->img.pos.x = 50; pS->img.pos.y = 215; 
+	pS->img.pos.w = 500; pS->img.pos.h = 400; 
 }
 
-ship_t get_ship(FILE *shipfile)
-{
-    // Parse the ship files
+void update_ship(ship_t pS, SDL_Renderer * pR){
+//
+	//printf("affichage du vaisseau\n");
+	SDL_QueryTexture(pS.img.tex,NULL,NULL,&pS.img.pos.w, &pS.img.pos.h);
+	SDL_RenderCopy(pR, pS.img.tex, NULL, &pS.img.pos);
 }
 
-ship_t * dummy_ship()
-{
-    ship_t *dummy = (ship_t *)malloc(sizeof(ship_t));
-    dummy->name = "hey";
-    dummy->is_shop = false;
-    dummy->hp = 1;
-    dummy->belongings = (belongings_t *)malloc(sizeof(belongings_t));
-    dummy->damage_min = 0;
-    dummy->damage_max = 1;
-    dummy->dodge_score = 0.1;
-    dummy->shield = 1;
-
-    return dummy;
+void free_ship(ship_t * pS){
+	printf("suppression du vaisseau\n");
+	SDL_FreeSurface(pS->img.img);
+	SDL_DestroyTexture(pS->img.tex);
 }
+
