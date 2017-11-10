0241f32d8fccb35cf6543256fad0740acfb42ae8
h0g <rodrigue.geis@icloud.com>
2017-11-10 11:12:18 +0100

    extended get_fonts.sh to get_ressources.sh

--- get_ressources.sh
+++ get_ressources.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 # Download custom fonts
 
+# Ressources URLs
+# Fonts
+INTER_UI_URL="https://github.com/rsms/inter/releases/download/v2.1/Inter-UI-2.1.zip"
+# MIDI
+MIDI_URL="https://www.dropbox.com/sh/l28n6wzh9l4guvr/AADJipJ9WZv6PWnQXW8N78T9a?dl=1"
+
+# Ressources folders
+FONTS_FOLDER=../assets/fonts
+MIDI_FOLDER=../assets/midi
+
+# Initialisation
 # Look for wget
 if [[ "$(which wget)" == "" ]]; then
     echo "wget not found in PATH. Please install it."
     exit 1
 fi
-
-INTER_UI_URL="https://github.com/rsms/inter/releases/download/v2.1/Inter-UI-2.1.zip"
-FONTS_FOLDER=../assets/fonts
 
 # Create necessary folders, download, extract, and remove temp files
 if [ ! -f $FONTS_FOLDER/Inter-UI-Regular.ttf ]; then
@@ -16,7 +24,7 @@ if [ ! -f $FONTS_FOLDER/Inter-UI-Regular.ttf ]; then
     cd $FONTS_FOLDER
     echo "Downloading fonts..."
     wget --no-verbose -O inter.zip $INTER_UI_URL
-    tar xvf inter.zip "Inter UI (TTF)/*"
+    tar xvzf inter.zip "Inter UI (TTF)/*"
     mv ./Inter\ UI\ \(TTF\)/*.ttf .
     rm -r "Inter UI (TTF)/" inter.zip
 fi
