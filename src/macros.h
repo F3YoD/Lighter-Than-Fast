d9154ffe6a5f5c012df3e4e2b08f5f489912d4b3
h0g <rodrigue.geis@icloud.com>
2017-11-30 21:44:14 +0100

    added macros specific to SDL sub-libs

--- src/macros.h
+++ src/macros.h
@@ -14,12 +14,43 @@
         {\
             fprintf(stderr, "%s initialization failed: %s\n",\
                     #variable, SDL_GetError());\
+            TTF_Quit();                                                 \
+            IMG_Quit();                                                 \
             SDL_Quit(); /* This frees memory for everything SDL-related */\
-            IMG_Quit();\
             exit(EXIT_FAILURE);\
         }\
         else if (PRINT_DEBUG)\
             printf("%s initialization success\n", #variable);\
+    }
+
+#define check_IMG(variable)                                             \
+    {                                                                   \
+        if (variable == NULL)                                           \
+        {                                                               \
+            fprintf(stderr, "%s initialization failed: %s\n",           \
+                    #variable, IMG_GetError());                         \
+            TTF_Quit();                                                 \
+            IMG_Quit();                                                 \
+            SDL_Quit(); /* This frees memory for everything SDL-related */ \
+            exit(EXIT_FAILURE);                                         \
+        }                                                               \
+        else if (PRINT_DEBUG)                                           \
+            printf("%s initialization success\n", #variable);           \
+    }
+
+#define check_TTF(variable)                                             \
+    {                                                                   \
+        if (variable == NULL)                                           \
+        {                                                               \
+            fprintf(stderr, "%s initialization failed: %s\n",           \
+                    #variable, TTF_GetError());                         \
+            TTF_Quit();                                                 \
+            IMG_Quit();                                                 \
+            SDL_Quit(); /* This frees memory for everything SDL-related */ \
+            exit(EXIT_FAILURE);                                         \
+        }                                                               \
+        else if (PRINT_DEBUG)                                           \
+            printf("%s initialization success\n", #variable);           \
     }
 
 #endif
