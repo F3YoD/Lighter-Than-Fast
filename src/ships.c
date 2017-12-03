12a333a3905178a8f6915eaa781dd6b29801cd1f
paq1 <p.paquin1016@laposte.net>
2017-12-03 21:38:15 +0100

    pierre : mise en place de la destruction du vaisseau du joueur

--- src/ships.c
+++ src/ships.c
@@ -11,12 +11,29 @@ void load_player_ship(player_ship_t * pS, char pDest[50]){
 	pS->img.tex = IMG_LoadTexture(renderer, pDest);
 	pS->img.pos.x = 100;
 	pS->img.pos.y = 100;
+
+	// ================== INIT STATS PLAYER ================== //
+	pS->ship.hp = 100;
+	pS->ship.shield = 50;
+	/***********************************************************/
 }
 
 void update_player_ship(player_ship_t * pS){
 // role : met a jour, affiche notre ship
+	// ======================= DRAW SHIP PLAYER =================== //
 	SDL_QueryTexture(pS->img.tex,NULL,NULL,&pS->img.pos.w, &pS->img.pos.h);
 	SDL_RenderCopy(renderer, pS->img.tex, NULL, &pS->img.pos);
+	/****************************************************************/
+
+	// ====================== UPDATE LIFE PLAYER ================== //
+	// juste pour le test de defaite du joueur
+	if(pS->ship.hp > 0) { pS->ship.hp--; }
+
+	if(pS->ship.hp <= 0){
+		if(pS->img.pos.x > -1000)
+			pS->img.pos.x--;
+	}
+	/****************************************************************/ 
 }
 
 void free_player_ship(player_ship_t * pS){
