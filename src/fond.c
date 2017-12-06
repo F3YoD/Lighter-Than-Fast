5747d808e445ed61926af03273bb4e4a1bce5b14
paq1 <p.paquin1016@laposte.net>
2017-12-06 17:05:08 +0100

    pierre : fond

--- src/fond.c
+++ src/fond.c
@@ -20,14 +20,19 @@ void load_fond(fond_t * pF){
 
 	pF->fond_right.tex = IMG_LoadTexture(renderer,
 	"../assets/images/Interface/Right.png");
+
+	pF->fond_bottom.tex = IMG_LoadTexture(renderer,
+	"../assets/images/Interface/Bot.png");
 	
 	pF->fond.pos.x = 0; pF->fond.pos.y = 0;
 	pF->fond_top.pos.x = 0; pF->fond_top.pos.y = -215;  
 	pF->fond_right.pos.x = 1024; pF->fond_right.pos.y = 48;  
+	pF->fond_bottom.pos.x = 0; pF->fond_bottom.pos.y = 768;  
 }
 
 void update_fond(fond_t * pF){
 //
+	printf("affichage du fond\n");
 	SDL_QueryTexture(pF->fond.tex,NULL,NULL,&pF->fond.pos.w, 
 			&pF->fond.pos.h);
 	SDL_RenderCopy(renderer, pF->fond.tex, NULL, &pF->fond.pos);
@@ -40,10 +45,16 @@ void update_fond(fond_t * pF){
 			&pF->fond_right.pos.h);
 	SDL_RenderCopy(renderer, pF->fond_right.tex, NULL, &pF->fond_right.pos);
 
+	SDL_QueryTexture(pF->fond_bottom.tex, NULL, NULL, &pF->fond_bottom.pos.w,
+			&pF->fond_bottom.pos.h);
+	SDL_RenderCopy(renderer, pF->fond_bottom.tex, NULL, &pF->fond_bottom.pos);
+	// h 152
 	if (pF->fond_top.pos.y < 0)
 		pF->fond_top.pos.y++;
 	if (pF->fond_right.pos.x > 1024 - 366)
 		pF->fond_right.pos.x--;  
+	if (pF->fond_bottom.pos.y > 768 - 152)
+		pF->fond_bottom.pos.y--;
 }
 
 void free_fond(fond_t * pF){
--- src/fond.h
+++ src/fond.h
@@ -9,6 +9,7 @@ typedef struct {
 	image_t fond;
 	image_t fond_top;
 	image_t fond_right;
+	image_t fond_bottom;
 }fond_t;
 
 void load_fond(fond_t *);
