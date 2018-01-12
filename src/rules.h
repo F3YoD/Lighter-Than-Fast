c8b760b1f1cda8e5a253403cf58c33bde22a1373
F3YoD <y.djama@hotmail.fr>
2018-01-12 01:10:54 +0100

    équilibrage

--- src/rules.h
+++ src/rules.h
@@ -1,8 +1,8 @@
 #ifndef _RULES_H
 #define _RULES_H
 
-#define RULE_COMBAT_ATTACK_COST 5
-#define RULE_COMBAT_REPAIR_COST 1
+#define RULE_COMBAT_ATTACK_COST 7
+#define RULE_COMBAT_REPAIR_COST 15
 #define RULE_COMBAT_FLEE_COST 10
 
 #endif /* _RULES_H */
